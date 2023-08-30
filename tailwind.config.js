43f209988200c56049088a2106a7f4b451c6d028
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-08-30 15:50:44 +0300

    Add-new-extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    '../components/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './style/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -13,11 +13,17 @@ module.exports = {
         'primary': '#febc69',
         'light-gray': 'rgba(195, 195, 197, 0.2)',
         'dark-gray': '#1e1e20',
+        'card-bg': '#222223',
         'white-1': '#fffffff0',
-        'white-2': '#ffffff91',
+        'white-2': '#ffffffcc',
+        'white-3': '#ffffff78',
       },
       boxShadow: {
-        'default': '-1px -1px 0px 0px #e7e7e752',
+        'default': '-1px -1px 0px 0px #3c3c3c',
+      },
+      fontFamily: {
+        'header': 'Inconsolata, monospace',
+        'links-1': 'Titillium Web, sans-serif',
       },
     },
   },
