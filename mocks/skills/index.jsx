82d32ad6f99da6adff52e7c16ecd5176d9829a22
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-09-02 02:12:58 +0300

    create-mocks-for-data

---
+++ mocks/skills/index.jsx
@@ -0,0 +1,76 @@
+const Skills = [
+    {
+        id: 1,
+        image: 'skills/react.png',
+        title: 'React JS',
+        url:'https://react.dev/learn'
+    },
+    {
+        id: 2,
+        image: 'skills/tailwind.png',
+        title: 'Tailwind CSS',
+        url:'https://tailwindcss.com/'
+    },
+    {
+        id: 3,
+        image: 'skills/javascript.png',
+        title: 'JavaScript',
+        url:'https://www.javascript.com/'
+    },
+    {
+        id: 4,
+        image: 'skills/nextjs.png',
+        title: 'Next JS',
+        url:'https://nextjs.org/'
+    },
+    {
+        id: 5,
+        image: 'skills/php.png',
+        title: 'PHP',
+        url:'https://www.php.net/docs.php'
+    },
+    {
+        id: 6,
+        image: 'skills/typescript.png',
+        title: 'typescript',
+        url:'https://www.typescriptlang.org/'
+    },
+    {
+        id: 7,
+        image: 'skills/photoshop.png',
+        title: 'Photoshop',
+        url:'https://www.adobe.com/tr/products/photoshop.html'
+    },
+    {
+        id: 8,
+        image: 'skills/illustrator.png',
+        title: 'Illustrator',
+        url:'https://www.adobe.com/tr/products/illustrator.html'
+    },
+    {
+        id: 9,
+        image: 'skills/premiere-pro.png',
+        title: 'Premiere Pro',
+        url:'https://www.adobe.com/tr/products/premiere.html'
+    },
+    {
+        id: 10,
+        image: 'skills/git.png',
+        title: 'Git',
+        url:'https://git-scm.com/'
+    },
+    {
+        id: 11,
+        image: 'skills/figma.png',
+        title: 'Figma',
+        url:'https://www.figma.com/'
+    }
+    
+]
+
+
+
+
+
+
+export { Skills }
