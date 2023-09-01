bcc0c9acaa040a97a66fb0c913e6c54e6e3610a0
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-09-01 11:35:30 +0300

    header-update

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 function index() {
   return (
-<div className='backdrop-blur-blur-1 fixed lg:absolute bottom-0 left-1/2 transform -translate-x-1/2 lg:top-0 lg:right-0 w-full lg:w-3/5 bg-light-gray rounded-tr-3xl rounded-tl-3xl'>
+<div className='backdrop-blur-blur-1 fixed lg:absolute bottom-0 left-0 lg:bottom-auto lg:left-auto lg:bottom- lg:top-0 lg:right-0 w-full lg:w-3/5 bg-light-gray rounded-tr-3xl rounded-tl-3xl lg:rounded-tl-none lg:rounded-bl-3xl'>
             <ul className='flex justify-evenly lg:justify-between'>
               <li className='py-5 px-2'>
                 <Link href="/" className='text-white-2 font-links-1 font-semibold text-sm lg:text-lg hover:text-white-3 transition'>About</Link>
