ea684e9e5b01afdd2d768ac57b54c7dbf94d714e
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-09-03 04:13:53 +0300

    create-portfolio

---
+++ components/portfolio/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {HeadTitle, Works } from '../index.js';
+import {Work} from '../../mocks/work-data';
+
+
+function Portfolio() {
+  
+
+  return (
+    <div className='mt-6 ml-6'>
+      <HeadTitle children='Portfolio' />
+      <div className='flex flex-wrap justify-between mr-5'>
+        {Work.map((Work) => (
+        <Works key={Work.id} title={Work.title} subtitle={Work.subtitle} image={Work.image}/>
+         ))}  
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
