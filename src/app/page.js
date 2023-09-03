e73769552b2e0a122fd4fa60779e40b7235fab12
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-09-03 04:13:20 +0300

    some-updates-on-page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
-import { Sidebar, About, Resume, Header } from '../../components'
-
+"use client"
+import React from 'react'
+import { Sidebar, About, Resume, Header, Portfolio } from '../../components'
+import { usePathname } from 'next/navigation'
 
 export default function Home()
 {
+
+  const { pathname } = usePathname()
+
+
   return (
     <div className='w-full'>
       <div className="lg:w-3/4 w-full mx-auto pt-5  lg:py-16 flex flex-col lg:flex-row justify-between">
@@ -10,7 +16,9 @@ export default function Home()
         <div className="lg:w-[75%] w-full bg-dark-gray lg:h-full border rounded-3xl border-light-gray relative lg:mt-0 mt-6 mx-5 lg:mb-0 mb-20">
           <Header />
           {/* <About /> */}
-          <Resume />
+          {/* <Resume /> */}
+          <Portfolio />
+
 
         </div>
       </div>
