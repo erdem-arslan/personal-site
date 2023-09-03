b28fea1e60896f397d6b233cfc8fb8eb103b3ca1
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-09-03 04:14:17 +0300

    add-works-component

---
+++ mocks/work-data/index.jsx
@@ -0,0 +1,41 @@
+const Work = [
+    {
+        id: 2,
+        image: 'works/galatasaray.png',
+        title: 'Galatasaray Filter',
+        subtitle:'Creative Design | AR/VR'
+    },
+    {
+        id: 1,
+        image: 'works/holbaslar.png',
+        title: 'Holbaşlar İnşaat',
+        subtitle:'Web Development'
+    },
+    {
+        id: 3,
+        image: 'works/arinresort.png',
+        title: 'Arin Resort',
+        subtitle:'Web Development'
+    },
+    {
+        id: 4,
+        image: 'works/diner.png',
+        title: 'Diner',
+        subtitle:'Web Development'
+    },
+    {
+        id: 5,
+        image: 'works/kent.png',
+        title: 'Kent Labaratuvar',
+        subtitle:'Web Development'
+    },
+    
+    
+]
+
+
+
+
+
+
+export { Work }
