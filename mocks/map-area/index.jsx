24c28efff7b07e45a733152bba8dbe0ad996ea68
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-09-04 22:47:40 +0300

    add-new-data

---
+++ mocks/map-area/index.jsx
@@ -0,0 +1,7 @@
+const MapArea = [
+    {
+        id: 1,
+        url: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24379.133175283543!2d29.051375460178036!3d40.20035021632278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14ca1582d8d45695%3A0x61a00555fc973392!2sBursa!5e0!3m2!1str!2str!4v1693734031447!5m2!1str!2str',
+    },
+]
+export { MapArea }
