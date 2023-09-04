f3be9522ebbffd04e5dfa1a33950aee97723759e
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-09-04 22:48:41 +0300

    update-works-component

--- components/works/index.jsx
+++ components/works/index.jsx
@@ -1,41 +1,18 @@
-import React, { useState } from 'react';
-import { AiFillEye } from 'react-icons/ai';
+import React from 'react';
 import ModalImage from 'react-modal-image';
 
 
 
 function index({image, title, subtitle}) {
-    const [showEye, setShowEye] = useState(false);
-
-  const handleMouseEnter = () => {
-    setShowEye(true);
-  };
-
-  const handleMouseLeave = () => {
-    setShowEye(false);
-  };
-
-  return (
+    return (
     <div className='w-full md:w-[30%] mb-14'>
-          <div className='relative' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-              {showEye && (
-                <div>
-                    <div className=''></div>
-                    <AiFillEye size={80} className='text-primary text-3xl z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'/>
-                </div>
-              )}
-              <ModalImage
-                className='w-full h-48 object-top object-cover cursor-pointer hover:p-1 opacity-70 rounded-3xl'
-                small={image}
-                large={image}
-                hideDownload={true}
-                alt={title}
-                
-              />
-              </div>
+          <div className='relative'>
+                <ModalImage className='w-full h-48 object-top object-cover cursor-pointer hover:p-1 hover:opacity-50 opacity-80 transition rounded-3xl '
+                small={image} large={image} hideDownload={true} alt={title}/>
+          </div>
           <div className='ml-2 mt-1'>
-            <h3 className='font-links-1 font-semibold text-2xl text-white-1'>{title}</h3>
-            <p className='font-links-1 text-white-3 '>{subtitle}</p>
+                <h3 className='font-links-1 font-semibold text-2xl text-white-1'>{title}</h3>
+                <p className='font-links-1 text-white-3 '>{subtitle}</p>
           </div>
         </div>
   )
