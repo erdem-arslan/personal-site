9c245d92819e5a0311392c33d2e335935a7779ab
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-09-04 22:48:13 +0300

    update-pagejs

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,21 @@
 "use client"
 import React from 'react'
-import { Sidebar, About, Resume, Header, Portfolio } from '../../components'
-import { usePathname } from 'next/navigation'
+import { Sidebar, About, Resume, Header, Portfolio, Contact } from '../../components'
 
 export default function Home()
 {
 
-  const { pathname } = usePathname()
-
 
   return (
     <div className='w-full'>
-      <div className="lg:w-3/4 w-full mx-auto pt-5  lg:py-16 flex flex-col lg:flex-row justify-between">
+      <div className="lg:w-3/4 w-full px-5 mx-auto pt-5 lg:py-16 flex flex-col lg:flex-row justify-between">
         <Sidebar />
-        <div className="lg:w-[75%] w-full bg-dark-gray lg:h-full border rounded-3xl border-light-gray relative lg:mt-0 mt-6 mx-5 lg:mb-0 mb-20">
+        <div className="lg:w-[75%] w-full bg-dark-gray lg:h-full border rounded-3xl border-light-gray relative lg:mt-0 mt-6 px-5 lg:mb-0 mb-20">
           <Header />
           {/* <About /> */}
           {/* <Resume /> */}
           <Portfolio />
-
-
+          {/* <Contact /> */}
         </div>
       </div>
     </div>
